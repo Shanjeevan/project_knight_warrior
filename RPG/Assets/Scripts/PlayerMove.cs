a2f6bb419a478c414a25fa1f3401e3d267c8724c
Shanjeevan_Mukunthan <Shanjee1997@gmail.com>
2023-02-16 11:36:27 +0530

    character_movement

--- RPG/Assets/Scripts/PlayerMove.cs
+++ RPG/Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class PlayerMove : MonoBehaviour
 {
+
+    private NavMeshAgent nav;
+
+    private Ray ray;
+
+    private RaycastHit hit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {      
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+          
+            if (Physics.Raycast(ray,out hit)) 
+            {         
+                /*nav.SetDestination(hit.point);*/
+                nav.destination = hit.point;
+            }
+        }
     }
 }
