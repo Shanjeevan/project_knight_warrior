3c9f6ff99b01526c673472215ceb4887843258f4
Shanjeevan_Mukunthan <Shanjee1997@gmail.com>
2023-02-17 17:04:48 +0530

    Camera_following

--- RPG/Assets/Scripts/PlayerMove.cs
+++ RPG/Assets/Scripts/PlayerMove.cs
@@ -2,22 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Cinemachine;
 public class PlayerMove : MonoBehaviour
 {
 
     private NavMeshAgent nav;
+    //we need to talk with animator
     private Animator anim;
     private Ray ray;
     private RaycastHit hit;
     private float x;
     private float z;
     private float velocitySpeed;
+    private CinemachineTransposer ct;
+    public CinemachineVirtualCamera playerCam;
+    //mouseposition
+    private Vector3 pos;
+    //current position
+    private Vector3 currPos;
+   
+
 
     // Start is called before the first frame update
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+
         anim = GetComponent<Animator>();
+        ct = playerCam.GetCinemachineComponent<CinemachineTransposer>();
+        currPos = ct.m_FollowOffset;
     }
 
     // Update is called once per frame
@@ -28,10 +41,9 @@ public class PlayerMove : MonoBehaviour
         z = nav.velocity.z;
         velocitySpeed = x + z;
 
-
-
-
-
+        //mouse position
+        pos = Input.mousePosition;
+      
         if (Input.GetMouseButtonDown(0))
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -52,5 +64,15 @@ public class PlayerMove : MonoBehaviour
         {
             anim.SetBool("sprinting", false);
         }
+
+        if (Input.GetMouseButton(1))
+        {
+            if (pos.x != 0 || pos.y != 0)
+            {
+                currPos = pos / 200;
+            }
+        }
+
+        ct.m_FollowOffset = currPos;
     }
 }
